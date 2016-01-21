5eb82b48951ffb721929b8f0b705c2d78ef59d86
Pablo Tortorella <pablotortorella@gmail.com>
2016-01-21 16:44:10 -0300

    nuevas landing pages

--- app.rb
+++ app.rb
@@ -9,3 +9,15 @@ end
 get '/facilitacion-grafica' do
 	erb :fg
 end
+
+get '/innovacion-creativiad' do
+	erb :innovacion-creatividad
+end
+
+get '/planificacion-estrategica' do
+	erb :planificacion-estrategica
+end
+
+get 'dinamicas-eventos' do
+	erb :dinamicas-eventos
+end
